d24ebec59315af4ae3df027fb453c6ed62c70674
bubble <xyu92315@gmail.com>
2026-04-28 00:12:16 +0800

    conflict main.py

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 def main():
     print("Hello from git-test!")
 
+def conflict_example():
+    print("I think this is a conflict example.")
 
 if __name__ == "__main__":
     main()
+    conflict_example()
